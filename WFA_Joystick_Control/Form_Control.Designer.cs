035ebb5737931d6759eadb815241bcaeb12743ee
Leonid133 <lenin133@yandex.ru>
2015-09-19 15:59:27 +0300

    First Commit

---
+++ WFA_Joystick_Control/Form_Control.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form_Control
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.joystickTimer = new System.Windows.Forms.Timer(this.components);
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "left";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(109, 75);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "right";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(59, 46);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "up";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(59, 104);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "down";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.OrangeRed;
+            this.button6.Location = new System.Drawing.Point(12, 143);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Record";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(128, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Refresh";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // joystickTimer
+            // 
+            this.joystickTimer.Enabled = true;
+            this.joystickTimer.Tick += new System.EventHandler(this.joystickTimer_Tick);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(944, 506);
+            this.webBrowser1.TabIndex = 8;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted_1);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(13, 183);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Joystic_Connect";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form_Control
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 506);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form_Control";
+            this.Text = "Управление";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Timer joystickTimer;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ WFA_Joystick_Control/Form_Control.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Security.Permissions;
+
+//using Microsoft.DirectX.DirectInput;
+
+using System.Threading;
+using System.IO;
+using System.Management;
+
+using System.Drawing.Imaging;
+using System.Collections;
+
+
+
+namespace WindowsFormsApplication1
+{
+    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
+    //[FlagsAttribute]
+
+    
+    /*
+    [FlagsAttribute]
+    [ComVisibleAttribute(true)]
+    [TypeConverterAttribute(typeof(KeysConverter))]
+    public enum Keys
+        */
+    public partial class Form_Control : Form
+    {
+        private Joystick joystick;
+        private bool[] joystickButtons;
+
+        //String ADRESS = "http:\\\\192.168.1.163:80";
+
+        public Form_Control()
+        {
+            InitializeComponent();
+            joystick = new Joystick(this.Handle);
+            connectToJoystick(joystick);
+        }
+        //---------------------------------------------------------------------
+        private void connectToJoystick(Joystick joystick)
+        {
+            while (true)
+            {
+                string sticks = joystick.FindJoysticks();
+                if (sticks != null)
+                {
+                    if (joystick.AcquireJoystick(sticks))
+                    {
+                        enableTimer();
+                        break;
+                    }
+                }
+                else
+                    break;
+            }
+        }
+
+        private void enableTimer()
+        {
+            if (this.InvokeRequired)
+            {
+                BeginInvoke(new ThreadStart(delegate()
+                {
+                    joystickTimer.Enabled = true;
+                }));
+            }
+            else
+                joystickTimer.Enabled = true;
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //webBrowser1.Navigate("http://192.168.1.163:80/");
+            //webBrowser1.Refresh();
+            webBrowser1.Url = new Uri("http://192.168.1.163:80/");
+            
+
+        }
+        public void Test(String message)
+        {
+            MessageBox.Show(message, "client code");
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //webBrowser1.AllowWebBrowserDrop = false;
+            //webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            //webBrowser1.WebBrowserShortcutsEnabled = false;
+            webBrowser1.ObjectForScripting = this;
+            // Uncomment the following line when you are finished debugging.
+        //webBrowser1.ScriptErrorsSuppressed = true;
+            /*
+             webBrowser1.DocumentText =
+            "<html><head><script>" +
+            "function test(message) { alert(message); }" +
+            "</script></head><body><button " +
+            "onclick=\"window.external.Test('called from script code')\">" +
+            "call client code from script code</button>" +
+            "</body></html>";*/
+            /*
+             webBrowser1.DocumentText =
+             "<html><head></head><body><img src=\"C:\\Users\\User\\Documents\\Visual Studio 2010\\Projects\\WindowsFormsApplication1\\WindowsFormsApplication1\\1.jpg\" style=\"width:640px;height:480px;\"> "  +
+             "</body></html>";
+            */
+            string directory = AppDomain.CurrentDomain.BaseDirectory;
+             webBrowser1.DocumentText =
+             "<html><head></head><body><img src=\"" + 
+             directory +
+             "1.jpg\" style=\"width:640px;height:480px;\"> " +
+             "</body></html>";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Document.InvokeScript("Button_onclick",
+     new String[] { "left" });
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Document.InvokeScript("Button_onclick",
+    new String[] { "right" });
+            //webBrowser1.Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Document.InvokeScript("Button_onclick",
+    new String[] { "up" });
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Document.InvokeScript("Button_onclick",
+    new String[] { "down" });
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            //MessageBox.Show("Pressed " + Keys.Shift);
+        }
+
+        private void joystickTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                joystick.UpdateStatus();
+                joystickButtons = joystick.buttons;
+
+                if (joystick.Xaxis == 0)
+                    webBrowser1.Document.InvokeScript("Button_onclick", new String[] { "left" });//output.Text += "Left\n";
+
+                if (joystick.Xaxis == 65535)
+                    webBrowser1.Document.InvokeScript("Button_onclick", new String[] { "right" });  //output.Text += "Right\n";
+
+                if (joystick.Yaxis == 0)
+                    webBrowser1.Document.InvokeScript("Button_onclick", new String[] { "up" }); //output.Text += "Up\n";
+
+                if (joystick.Yaxis == 65535)
+                    webBrowser1.Document.InvokeScript("Button_onclick", new String[] { "down" }); //output.Text += "Down\n";
+
+                for (int i = 0; i < joystickButtons.Length; i++)
+                {
+                    if (joystickButtons[i] == true)
+                    {
+                        webBrowser1.Url = new Uri("http://192.168.1.163:80/"); //output.Text += "Button " + i + " Pressed\n";
+                        //webBrowser1.Refresh();
+                    }
+                }
+            }
+            catch
+            {
+                joystickTimer.Enabled = false;
+                connectToJoystick(joystick);
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted_1(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            connectToJoystick(joystick);
+        }
+       
+    }
+}
