c6f88593e6f622cb46e0ce2ffa43b78713cb3d4b
ln.blohin <ln.blohin@tensor.ru>
2015-09-24 19:58:36 +0300

    ОТладка TcpIp
    Signed-off-by: ln.blohin <ln.blohin@tensor.ru>

--- WFA_Joystick_Control/Form_Control.cs
+++ WFA_Joystick_Control/Form_Control.cs
@@ -126,9 +126,10 @@ namespace WFA_Joystick_Control
         {
             //webBrowser1.Navigate("http://192.168.1.163:80/");
             //webBrowser1.Refresh();
-            //webBrowser1.Url = new Uri(ReadStringConnect());
+            webBrowser1.Url = new Uri(ReadStringConnect());
             string str = "Ok!";
             String message = "Ok";
+            
             message = laurent1.ConnectToLaurent();
             MessageBox.Show(message, str);
             message = laurent1.LoginToLaurent();
