fab0dcc461046d5d1de40c1933a573355e6fa390
ln.blohin <ln.blohin@tensor.ru>
2015-09-27 17:57:18 +0300

    Controlls

--- WFA_Joystick_Control/Controlls.cs
+++ WFA_Joystick_Control/Controlls.cs
@@ -14,39 +14,217 @@ namespace WFA_Joystick_Control
         public void UpOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
         {
             tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("1");
         }
         public void UpOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
         {
             tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("1");
         }
 
         public void DownOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
         {
             tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("2");
         }
         public void DownOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
         {
             tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("2");
         }
 
         public void LeftOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
         {
             tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("8");
         }
         public void LeftOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
         {
             tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("8");
         }
 
         public void RightOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
         {
             tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("7");
         }
         public void RightOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
         {
             tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("7");
+        }
+        public void FoldingUpOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("9");
+        }
+        public void FoldingUpOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("9");
+        }
+        public void FoldingDownOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("10");
+        }
+        public void FoldingDownOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("10");
+        }
+        public void FixFoldingUpOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorB.OnRel("11");
+        }
+        public void FixFoldingUpOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorB.OffRel("11");
+        }
+        public void GunUpOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("5");
+        }
+        public void GunUpOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("5");
         }
 
+        public void GunDownOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("6");
+        }
+        public void GunDownOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("6");
+        }
+
+        public void GunLeftOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("4");
+        }
+        public void GunLeftOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("4");
+        }
+
+        public void GunRightOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("3");
+        }
+        public void GunRightOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("3");
+        }
+        public void ProjectorOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorB.OnRel("12");
+        }
+        public void ProjectorOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorB.OffRel("12");
+        }
+        public void CamOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OnRel("12");
+        }
+        public void CamOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OffRel("12");
+        }
+        public void OptionalEquipment1UpOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("4");
+        }
+        public void OptionalEquipment1UpOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("4");
+        }
+
+        public void OptionalEquipment1DownOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("5");
+        }
+        public void OptionalEquipment1DownOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("5");
+        }
+        public void OptionalEquipment2UpOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("6");
+        }
+        public void OptionalEquipment2UpOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("6");
+        }
+
+        public void OptionalEquipment2DownOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("7");
+        }
+        public void OptionalEquipment2DownOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("7");
+        }
+        public void OptionalEquipment3UpOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("8");
+        }
+        public void OptionalEquipment3UpOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("8");
+        }
+
+        public void OptionalEquipment3DownOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("9");
+        }
+        public void OptionalEquipment3DownOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("9");
+        }
+        public void OptionalEquipment4UpOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("10");
+        }
+        public void OptionalEquipment4UpOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("10");
+        }
+
+        public void OptionalEquipment4DownOn(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OnRel("1");
+            tcp_connectorB.OnRel("11");
+        }
+        public void OptionalEquipment4DownOff(ref TcpIpLaurentConnector tcp_connectorA, ref TcpIpLaurentConnector tcp_connectorB)
+        {
+            tcp_connectorA.OffRel("1");
+            tcp_connectorB.OffRel("11");
+        }
         ~Controlls()
         {
         }
--- WFA_Joystick_Control/Form_Control.Designer.cs
+++ WFA_Joystick_Control/Form_Control.Designer.cs
@@ -47,9 +47,9 @@
             this.textBox_VideoConnect = new System.Windows.Forms.TextBox();
             this.ReserveKeybord_timer = new System.Windows.Forms.Timer(this.components);
             this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
-            this.textBox_TCP_1 = new System.Windows.Forms.TextBox();
-            this.textBox_TCP_2 = new System.Windows.Forms.TextBox();
             this.textBox_Port2 = new System.Windows.Forms.TextBox();
+            this.textBox_TCP_2 = new System.Windows.Forms.TextBox();
+            this.textBox_TCP_1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.toolStripContainer1.ContentPanel.SuspendLayout();
             this.toolStripContainer1.SuspendLayout();
@@ -57,9 +57,10 @@
             // 
             // button_connect1
             // 
-            this.button_connect1.Location = new System.Drawing.Point(347, 33);
+            this.button_connect1.Location = new System.Drawing.Point(463, 41);
+            this.button_connect1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_connect1.Name = "button_connect1";
-            this.button_connect1.Size = new System.Drawing.Size(75, 23);
+            this.button_connect1.Size = new System.Drawing.Size(100, 28);
             this.button_connect1.TabIndex = 1;
             this.button_connect1.Text = "connect";
             this.button_connect1.UseVisualStyleBackColor = true;
@@ -67,9 +68,10 @@
             // 
             // button_left
             // 
-            this.button_left.Location = new System.Drawing.Point(6, 26);
+            this.button_left.Location = new System.Drawing.Point(8, 32);
+            this.button_left.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_left.Name = "button_left";
-            this.button_left.Size = new System.Drawing.Size(41, 23);
+            this.button_left.Size = new System.Drawing.Size(55, 28);
             this.button_left.TabIndex = 2;
             this.button_left.Text = "left";
             this.button_left.UseVisualStyleBackColor = true;
@@ -77,9 +79,10 @@
             // 
             // button_right
             // 
-            this.button_right.Location = new System.Drawing.Point(96, 26);
+            this.button_right.Location = new System.Drawing.Point(128, 32);
+            this.button_right.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_right.Name = "button_right";
-            this.button_right.Size = new System.Drawing.Size(41, 23);
+            this.button_right.Size = new System.Drawing.Size(55, 28);
             this.button_right.TabIndex = 3;
             this.button_right.Text = "right";
             this.button_right.UseVisualStyleBackColor = true;
@@ -87,9 +90,10 @@
             // 
             // button_up
             // 
-            this.button_up.Location = new System.Drawing.Point(52, 7);
+            this.button_up.Location = new System.Drawing.Point(69, 9);
+            this.button_up.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_up.Name = "button_up";
-            this.button_up.Size = new System.Drawing.Size(41, 23);
+            this.button_up.Size = new System.Drawing.Size(55, 28);
             this.button_up.TabIndex = 4;
             this.button_up.Text = "up";
             this.button_up.UseVisualStyleBackColor = true;
@@ -97,9 +101,10 @@
             // 
             // button_down
             // 
-            this.button_down.Location = new System.Drawing.Point(52, 50);
+            this.button_down.Location = new System.Drawing.Point(69, 62);
+            this.button_down.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_down.Name = "button_down";
-            this.button_down.Size = new System.Drawing.Size(41, 23);
+            this.button_down.Size = new System.Drawing.Size(55, 28);
             this.button_down.TabIndex = 5;
             this.button_down.Text = "down";
             this.button_down.UseVisualStyleBackColor = true;
@@ -108,9 +113,10 @@
             // button_Record
             // 
             this.button_Record.BackColor = System.Drawing.Color.OrangeRed;
-            this.button_Record.Location = new System.Drawing.Point(807, 36);
+            this.button_Record.Location = new System.Drawing.Point(1076, 44);
+            this.button_Record.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_Record.Name = "button_Record";
-            this.button_Record.Size = new System.Drawing.Size(75, 23);
+            this.button_Record.Size = new System.Drawing.Size(100, 28);
             this.button_Record.TabIndex = 6;
             this.button_Record.Text = "Record";
             this.button_Record.UseVisualStyleBackColor = false;
@@ -118,9 +124,10 @@
             // 
             // button_refresh
             // 
-            this.button_refresh.Location = new System.Drawing.Point(446, 7);
+            this.button_refresh.Location = new System.Drawing.Point(595, 9);
+            this.button_refresh.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_refresh.Name = "button_refresh";
-            this.button_refresh.Size = new System.Drawing.Size(75, 23);
+            this.button_refresh.Size = new System.Drawing.Size(100, 28);
             this.button_refresh.TabIndex = 7;
             this.button_refresh.Text = "Refresh";
             this.button_refresh.UseVisualStyleBackColor = true;
@@ -133,18 +140,20 @@
             // 
             // webBrowser1
             // 
-            this.webBrowser1.Location = new System.Drawing.Point(129, 95);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Location = new System.Drawing.Point(172, 117);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(27, 25);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(657, 344);
+            this.webBrowser1.Size = new System.Drawing.Size(876, 423);
             this.webBrowser1.TabIndex = 8;
             this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted_1);
             // 
             // button_joyConnect
             // 
-            this.button_joyConnect.Location = new System.Drawing.Point(6, 119);
+            this.button_joyConnect.Location = new System.Drawing.Point(8, 146);
+            this.button_joyConnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_joyConnect.Name = "button_joyConnect";
-            this.button_joyConnect.Size = new System.Drawing.Size(75, 23);
+            this.button_joyConnect.Size = new System.Drawing.Size(100, 28);
             this.button_joyConnect.TabIndex = 9;
             this.button_joyConnect.Text = "Joystic_Connect";
             this.button_joyConnect.UseVisualStyleBackColor = true;
@@ -152,27 +161,29 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(167, 11);
+            this.textBox1.Location = new System.Drawing.Point(223, 14);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(164, 20);
+            this.textBox1.Size = new System.Drawing.Size(217, 22);
             this.textBox1.TabIndex = 10;
             this.textBox1.Text = "http://192.168.1.163/";
             this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
             // 
             // textBox_Port1
             // 
-            this.textBox_Port1.Location = new System.Drawing.Point(280, 36);
-            this.textBox_Port1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_Port1.Location = new System.Drawing.Point(373, 44);
+            this.textBox_Port1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox_Port1.Name = "textBox_Port1";
-            this.textBox_Port1.Size = new System.Drawing.Size(51, 20);
+            this.textBox_Port1.Size = new System.Drawing.Size(67, 22);
             this.textBox_Port1.TabIndex = 12;
             this.textBox_Port1.Text = "2424";
             // 
             // buttonURLConnect
             // 
-            this.buttonURLConnect.Location = new System.Drawing.Point(347, 6);
+            this.buttonURLConnect.Location = new System.Drawing.Point(463, 7);
+            this.buttonURLConnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonURLConnect.Name = "buttonURLConnect";
-            this.buttonURLConnect.Size = new System.Drawing.Size(93, 23);
+            this.buttonURLConnect.Size = new System.Drawing.Size(124, 28);
             this.buttonURLConnect.TabIndex = 14;
             this.buttonURLConnect.Text = "URL_connect";
             this.buttonURLConnect.UseVisualStyleBackColor = true;
@@ -180,7 +191,8 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(129, 95);
+            this.pictureBox1.Location = new System.Drawing.Point(172, 117);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(449, 246);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -189,9 +201,10 @@
             // 
             // button_VideoConnect
             // 
-            this.button_VideoConnect.Location = new System.Drawing.Point(807, 7);
+            this.button_VideoConnect.Location = new System.Drawing.Point(1076, 9);
+            this.button_VideoConnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_VideoConnect.Name = "button_VideoConnect";
-            this.button_VideoConnect.Size = new System.Drawing.Size(75, 23);
+            this.button_VideoConnect.Size = new System.Drawing.Size(100, 28);
             this.button_VideoConnect.TabIndex = 16;
             this.button_VideoConnect.Text = "Video";
             this.button_VideoConnect.UseVisualStyleBackColor = true;
@@ -199,9 +212,10 @@
             // 
             // textBox_VideoConnect
             // 
-            this.textBox_VideoConnect.Location = new System.Drawing.Point(541, 10);
+            this.textBox_VideoConnect.Location = new System.Drawing.Point(721, 12);
+            this.textBox_VideoConnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox_VideoConnect.Name = "textBox_VideoConnect";
-            this.textBox_VideoConnect.Size = new System.Drawing.Size(260, 20);
+            this.textBox_VideoConnect.Size = new System.Drawing.Size(345, 22);
             this.textBox_VideoConnect.TabIndex = 17;
             this.textBox_VideoConnect.Text = "http://192.168.1.163/:8080/?action=snapshot";
             // 
@@ -235,49 +249,52 @@
             this.toolStripContainer1.ContentPanel.Controls.Add(this.button_left);
             this.toolStripContainer1.ContentPanel.Controls.Add(this.button_connect1);
             this.toolStripContainer1.ContentPanel.Controls.Add(this.pictureBox1);
-            this.toolStripContainer1.ContentPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(865, 434);
+            this.toolStripContainer1.ContentPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(1187, 534);
             this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
-            this.toolStripContainer1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.toolStripContainer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.toolStripContainer1.Name = "toolStripContainer1";
             this.toolStripContainer1.RightToolStripPanelVisible = false;
-            this.toolStripContainer1.Size = new System.Drawing.Size(890, 434);
+            this.toolStripContainer1.Size = new System.Drawing.Size(1187, 534);
             this.toolStripContainer1.TabIndex = 18;
             this.toolStripContainer1.Text = "toolStripContainer1";
             this.toolStripContainer1.TopToolStripPanelVisible = false;
             // 
-            // textBox_TCP_1
-            // 
-            this.textBox_TCP_1.Location = new System.Drawing.Point(167, 36);
-            this.textBox_TCP_1.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox_TCP_1.Name = "textBox_TCP_1";
-            this.textBox_TCP_1.Size = new System.Drawing.Size(86, 20);
-            this.textBox_TCP_1.TabIndex = 11;
-            this.textBox_TCP_1.Text = "192.168.0.110";
-            // 
-            // textBox_TCP_2
-            // 
-            this.textBox_TCP_2.Location = new System.Drawing.Point(167, 62);
-            this.textBox_TCP_2.Name = "textBox_TCP_2";
-            this.textBox_TCP_2.Size = new System.Drawing.Size(86, 20);
-            this.textBox_TCP_2.TabIndex = 18;
-            this.textBox_TCP_2.Text = "192.168.0.111";
-            // 
             // textBox_Port2
             // 
-            this.textBox_Port2.Location = new System.Drawing.Point(280, 61);
+            this.textBox_Port2.Location = new System.Drawing.Point(373, 75);
+            this.textBox_Port2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox_Port2.Name = "textBox_Port2";
-            this.textBox_Port2.Size = new System.Drawing.Size(51, 20);
+            this.textBox_Port2.Size = new System.Drawing.Size(67, 22);
             this.textBox_Port2.TabIndex = 19;
             this.textBox_Port2.Text = "2424";
             // 
+            // textBox_TCP_2
+            // 
+            this.textBox_TCP_2.Location = new System.Drawing.Point(223, 76);
+            this.textBox_TCP_2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox_TCP_2.Name = "textBox_TCP_2";
+            this.textBox_TCP_2.Size = new System.Drawing.Size(113, 22);
+            this.textBox_TCP_2.TabIndex = 18;
+            this.textBox_TCP_2.Text = "192.168.0.111";
+            // 
+            // textBox_TCP_1
+            // 
+            this.textBox_TCP_1.Location = new System.Drawing.Point(223, 44);
+            this.textBox_TCP_1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox_TCP_1.Name = "textBox_TCP_1";
+            this.textBox_TCP_1.Size = new System.Drawing.Size(113, 22);
+            this.textBox_TCP_1.TabIndex = 11;
+            this.textBox_TCP_1.Text = "192.168.0.110";
+            // 
             // Form_Control
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(890, 434);
+            this.ClientSize = new System.Drawing.Size(1187, 534);
             this.Controls.Add(this.toolStripContainer1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form_Control";
             this.Text = "Управление";
             this.Load += new System.EventHandler(this.Form1_Load);
