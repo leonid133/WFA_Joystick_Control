09d65e9384329c388a93640b7bf3d3a9d118cb78
ln.blohin <ln.blohin@tensor.ru>
2015-10-03 16:06:22 +0300

    Формочка Settings

---
+++ WFA_Joystick_Control/Setrtings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WFA_Joystick_Control
+{
+    public partial class Form_Settings : Form
+    {
+        public Form_Settings()
+        {
+            InitializeComponent();
+        }
+    }
+}
